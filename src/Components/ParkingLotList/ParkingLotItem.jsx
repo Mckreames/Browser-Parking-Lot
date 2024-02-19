a06f186f60c6be97799dd2866bd0a4f1fe9850f3
Mckreames <114367512+Mckreames@users.noreply.github.com>
2024-02-18 21:51:43 -0500

    reactstrap card component

--- src/Components/ParkingLotList/ParkingLotItem.jsx
+++ src/Components/ParkingLotList/ParkingLotItem.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Card, CardHeader, CardBody, CardTitle, CardText } from 'reactstrap';
 
 export default function ParkingLotItem({id, link, description, date, priority}) {
 
   return (
-    <section className="parking-lot-item-container">
-        <p className="parking-lot-item-field">Date: { date }</p>
+    <Card 
+    className="my-2 parking-lot-item-container"
+    color="dark"
+    inverse
+    >
+        <CardHeader>
+            { date } (priority: {priority})
+        </CardHeader>
+        <CardBody>
+            <CardTitle>
+                {description}
+            </CardTitle>
+            <CardText>
+                Link:&nbsp; <a href={link} target="_blank" className="parking-lot-item-anchor">
+                    {link}
+                </a>
+            </CardText>
+        </CardBody>
         <p className="parking-lot-item-field">Priority: { priority }</p>
         <p className="parking-lot-item-field">Description: { description }</p>
         <p className="parking-lot-item-field">
@@ -13,6 +30,6 @@ export default function ParkingLotItem({id, link, description, date, priority}) 
                 { link }
             </a>
         </p>
-    </section>
+    </Card>
   );
 }
